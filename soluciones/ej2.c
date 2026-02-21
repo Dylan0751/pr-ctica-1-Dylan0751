a30e7e01d287f76730f00921ce42188a5fe62b43
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:30:26 -0600

    Update ej2.c

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,11 @@
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 2
-    return 0;
-}
+/*
+Sin ejecutar determina cuál dirección es mayor:
+&a[4] o &a[5]
+
+Justifica numéricamente y luego verifica ejecutando. 
+
+Empleando la formula: a[i]=base + i x 4 (la base es constante)
+&a[4] = 4 x 4 = 16
+&a[5] = 5 x 4 = 20
+*/
