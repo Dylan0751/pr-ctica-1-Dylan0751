3fef7ca53d198627828d952db95510669e803ffb
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:37:43 -0600

    Update ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,19 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 3
+    int a[3];
+    int b[3];
+    int c[3];
+    for(int i = 0;i<3;i++){
+        printf("&a[%d]=%p\n", i,&a[i]); 
+    }
+     for(int i = 0;i<3;i++){
+        printf("&b[%d]=%p\n", i,&b[i]); 
+    }
+     for(int i = 0;i<3;i++){
+        printf("&c[%d]=%p\n", i,&c[i]); 
+    }
+    return 0;
+    //Si estan juntos en memoria puede ser que dependa de cada compilador
     return 0;
 }
