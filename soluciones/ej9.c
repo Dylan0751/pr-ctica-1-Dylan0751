d59e8311c1c55214a4e62f939589f5ad75fdeef5
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:47:53 -0600

    Update ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,20 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 9
+    int a[5];
+    printf("a[-1] = %p \n", &a[-1]);
+    printf("a[5] = %p \n", &a[5]);
     return 0;
+  
 }
+/*por qué compila:
+Porque no verifica limite del arreglo 
+
+por qué es peligroso:
+Es peligroso porque apunta antes del arreglo y por ende puede causar problemas 
+como leer basura no eliminada
+
+qué revela sobre memoria:
+Demuestra que la memoria sigue siendo continua
+
+*/
