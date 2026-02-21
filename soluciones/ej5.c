81eed3832486f94049f4c4422806da4e1d55a111
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:43:31 -0600

    Update ej5.c

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -1,6 +1,14 @@
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 5
-    return 0;
-}
+/*
+Un estudiante afirma:
+Si a[0] = 1000 entonces a[10] = 1010.
+Demuestra formalmente si es correcto.
+
+La formula a usar en este caso es la siguiente 
+a[i] = base + i x 4 (4 por el tipo de dato)
+a[0] = 1000 + 0 x 4 = 1000
+a[10] = 1000 + 10 x 4 = 1040
+Por lo tanto la afirmación es incorrecta. Seria correcta solo si el tamaño del tipo de
+dato fuera de 1 byte.
+
+*/
