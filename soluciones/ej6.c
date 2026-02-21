36487c4ca358c00ab3b6fde3549eee71a234a033
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:45:11 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,14 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 6
+    int m[2][4];
+    for(int i = 0;i<2;i++){
+        for(int j = 0; j<4; j++){
+            printf("m[%d][%d] = [%p] ",i,j, &m[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
+    //la formula usada es m[i][j]= base + ((i x columnas) + j)x 4
+    //El almacenamiento se da por filas
 }
