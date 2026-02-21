332e3f3f4bc873e4131db4f510f5133a1564e5e5
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:24:30 -0600

    Update ej11.c

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,17 @@
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 11
-    return 0;
-}
+/*
+Salida:
+&a[0] = 1000
+&a[1] = 1008
+&a[2] = 1016
+
+Determina:
+tamaño del tipo:
+El tamaño del tipo es de 8 bytes 
+
+tipo posible:
+Seguramente es de tipo double o long long
+
+fórmula usada:
+la formula usada es a[i] = base + i x 8
+*/
