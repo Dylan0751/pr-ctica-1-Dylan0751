ec679454f2c0e8ae74aba0828d4c694c021308e9
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:42:59 -0600

    Update ej4.c

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -1,6 +1,19 @@
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 4
-    return 0;
-}
+/*
+Salida:
+&a[0] = 5000
+&a[3] = 5012
+
+Calcula:
+tamaño del tipo
+dirección de a[5]
+fórmula usada
+la formula usada es &a[i]=base + i x 4
+El tamaño es de 4 bytes por ser un entero 
+&a[0] = 5000 + 0 x 4 = 5000
+&a[1] = 5000 + 1 x 4 = 5004
+&a[2] = 5000 + 2 x 4 = 5008
+&a[3] = 5000 + 3 x 4 = 5012
+&a[4] = 5000 + 4 x 4 = 5016
+&a[5] = 5000 + 5 x 4 = 5020
+*/
