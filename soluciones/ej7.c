c94fdc16adf8c3dac446e8a6d7ae07d84482d556
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-02-20 22:45:46 -0600

    Update ej7.c

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -1,6 +1,14 @@
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 7
-    return 0;
-}
+/*Sin ejecutar determina cual dirección es mayor:
+&m[0][3]
+&m[1][0]
+Justifica matemáticamente.
+
+la formula a emplear es la siguiente:
+&m[i][j]= base + ((i x columnas) + j) x 4, en este caso las columnas serian 4, y la base es
+constante (en este ejercicio), por lo tanto:
+&m[0][3]= ((0 x 4) + 3) x 4 = 12
+&m[1][0]= ((1 x 4) + 0) x 4 = 16
+Asi que m[1][0] > m[0][3]
+
+*/
